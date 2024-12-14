f94e9ccb4c5239b8193dd356b175d86d854661cc
Petko Dimitrov <petkodmail@gmail.com>
2024-12-14 19:59:30 +0200

    Update Programming_project.py: Making a screen

--- Programming_project.py
+++ Programming_project.py
@@ -1,3 +1,33 @@
+
+
+#Importing modules that we use
 import pygame
 import sys
 
+#Initializing pygame
+pygame.init()
+
+#Our main screen properties
+screen_width = 800
+screen_height = 800
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Programming Project")  # This is the name of our window
+
+
+def main():
+
+    running = True
+
+    while running:
+
+        for event in pygame.event.get():
+            #Stops the game and closes the window if we press the close button
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        
+        screen.fill((0, 0, 0))
+        pygame.display.update()
+
+
+main()
